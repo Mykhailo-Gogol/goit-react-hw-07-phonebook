6ad6e005fd0a422e71d4bfba72944bf05ff4a4dd
Mykhailo-Gogol <68640759+Mykhailo-Gogol@users.noreply.github.com>
2021-04-23 14:58:15 +0300

    add json-server

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import "./App.css";
+import axios from "axios";
 
 import Form from "./components/Form";
 import Filter from "./components/Filter";
 import ContactList from "./components/ContactList";
+
+axios.get("http://localhost:4000/contacts").then(({ data }) => {
+  console.log(data);
+});
 
 const App = () => {
   return (
